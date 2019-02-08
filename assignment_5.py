ea3bffaa361e659dfbe0120765c189981f92c7ad
Alex Yancey <alex@heuri.tech>
2019-02-08 14:31:03 -0800

    Initial work assignment 5

---
+++ assignment_5.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+
+root = Tk()
+
+lowest_label = Label(root, text="10", background="gray", height=4, width=8, font=("Courier", 24))
+lowest_label.place(x=400, y=150)
+
+
+root.geometry("1024x768")
+root.mainloop()
