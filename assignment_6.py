449cd09d2f85a23037f76b9438e7dff5894242a6
Alex Yancey <alexyancey3@gmail.com>
2019-02-08 18:36:30 -0800

    rename

--- assignment_6.py
+++ assignment_6.py
@@ -1,22 +1,79 @@
-from tkinter import *
+import random
 import time
 
-root = Tk()
+# fun with sorting
 
-canvas = Canvas(root)
-canvas.pack(fill=BOTH, expand=1)
 
-def create_rect(x, y, text):
+def is_sorted(l):
+    last = None
+    for o in l:
+        if last:
+            if o < last:
+                return False
+            last = o
+        else:
+            last = o
+    return True
 
-    return canvas.create_rectangle(15 + x, 15 + y, x + 75, y + 75)
-    #canvas.create_text(65 + x, 65 + y, text=text)
 
-nice = create_rect(100, 100, "hey")
+def bogo(l):
+    while not is_sorted(l):
+        random.shuffle(l)
+    return l
 
-for s in range(0, 100):
-    canvas.move(nice, s, s)
-    root.update()
-    time.sleep(0.1)
 
-root.geometry("1024x768")
-root.mainloop()
+def rlistn(n):
+    new_list = []
+    for i in range(n):
+        new_list.append(random.randint(1, 100))
+    return new_list
+
+
+# i think this is selection sort
+def my_sort(l):
+    left = []
+    for e in l:
+        if left:
+            i = len(left)
+            while True:
+                if i < 0:
+                    left.insert(0, e)
+                    break
+
+                m = left[i - 1]
+
+                if e > m:
+                    left.insert(i, e)
+                    break
+                else:
+                    i -= 1
+        else:
+            left.append(e)
+
+    return left
+
+
+
+test_list = rlistn(9)
+
+# c = None
+#
+# while True:
+#     if c:
+#
+#     else:
+#         c = l.pop(0)
+#     d = l.pop(0)
+#     input()
+
+print(test_list)
+
+before = time.time()
+print(bogo(list(test_list)))
+print("bogo took {} seconds".format(time.time() - before))
+
+print(test_list)
+
+before = time.time()
+print(my_sort(list(test_list)))
+print("my sort took {} seconds".format(time.time() - before))
