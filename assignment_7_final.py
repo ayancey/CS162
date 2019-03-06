b88c38f58c9183da5584788e46c9e046c12af649
Alex Yancey <alexyancey3@gmail.com>
2019-03-06 14:56:48 -0800

    blah

--- assignment_7_final.py
+++ assignment_7_final.py
@@ -3,6 +3,11 @@
 # Joseph Jess, CS162
 import random
 import curses
+import math
+
+# i tried to implement this myself but math is hard :(
+from bresenham import bresenham
+
 
 tiles = []
 
@@ -36,56 +41,85 @@ player_y = height // 2
 player_x = width // 2
 
 
-money_y = player_y
+money_y = player_y - 5
 money_x = player_x - 15
 
 dist_x = money_x - player_x
 dist_y = money_y - player_y
 
-print(dist_y, dist_x)
+
 
 # money
 tiles[money_y][money_x] = "$"
 
-# wall
+#wall
 for i in range(10):
     tiles[(player_y - 5) + i][player_x - 10] = "#"
 
 # player
-tiles[player_y][player_x] = "@"
-
-# up
-for i in range(5):
-    tiles[player_y - 1 -  i][player_x] = "|"
-
-# down
-for i in range(5):
-    tiles[player_y + i + 1][player_x] = "|"
-
-# left
-for i in range(5):
-    tiles[player_y][player_x -i - 1] = "-"
-
-# right
-for i in range(5):
-    tiles[player_y][player_x + i + 1] = "-"
-
-# up left
-for i in range(5):
-    tiles[player_y -1 - i][player_x -i - 1] = "-"
-
-# up right
-for i in range(5):
-    tiles[player_y -1 - i][player_x +i + 1] = "-"
 
 
-# bottom left
-for i in range(5):
-    tiles[player_y +1 +i][player_x -i - 1] = "-"
 
-# bottom right
-for i in range(5):
-    tiles[player_y +1 + i][player_x +i + 1] = "-"
+
+# for point in bresenham(player_x, player_y, money_x, money_y):
+#     tiles[point[1]][point[0]] = "#"
+#     print(point)
+
+#
+# while True:
+#     if target_x < money_x:
+#         target_x += 1
+#     elif target_x > money_x:
+#         target_x -= 1
+#
+#     if target_y < money_y:
+#         target_y += 1
+#     elif target_y > money_y:
+#         target_y -= 1
+#
+#     tiles[target_y][target_x] = "#"
+#
+#     if target_x == money_x:
+#         if target_y == money_y:
+#             break
+
+
+# print((target_x, target_y))
+# print((money_x, money_y))
+
+
+# # up
+# for i in range(5):
+#     tiles[player_y - 1 -  i][player_x] = "|"
+#
+# # down
+# for i in range(5):
+#     tiles[player_y + i + 1][player_x] = "|"
+#
+# # left
+# for i in range(5):
+#     tiles[player_y][player_x -i - 1] = "-"
+#
+# # right
+# for i in range(5):
+#     tiles[player_y][player_x + i + 1] = "-"
+#
+# # up left
+# for i in range(5):
+#     tiles[player_y -1 - i][player_x -i - 1] = "-"
+#
+# # up right
+# for i in range(5):
+#     tiles[player_y -1 - i][player_x +i + 1] = "-"
+#
+#
+# # bottom left
+# for i in range(5):
+#     tiles[player_y +1 +i][player_x -i - 1] = "-"
+#
+# # bottom right
+# for i in range(5):
+#     tiles[player_y +1 + i][player_x +i + 1] = "-"
 
 
 #
@@ -96,20 +130,68 @@ for i in range(5):
 #input()
 
 
+class Object(object):
+    def __init__(self, x, y, c):
+        self.x = x
+        self.y = y
+        self.c = c
 
-representation = ""
-
-for y, row in enumerate(tiles):
-    for x, column in enumerate(row):
-        #dist_x =
+    def draw(self):
+        stdscr.addstr(self.y, self.x, c)
 
 
-        #print((x, y))
-        # visible
-        representation += column
-    representation += "\n"
+class Tile(object):
+    def __init__(self):
+        pass
 
-print(representation)
+
+#representation = ""
+
+
+stdscr = curses.initscr()
+curses.noecho()
+curses.cbreak()
+stdscr.keypad(True)
+
+
+def draw_stuff():
+    for y, row in enumerate(tiles):
+        for x, column in enumerate(row):
+            blocked = False
+       # if column == "$":
+            if column != "#":
+                b = list(bresenham(player_x, player_y, x, y))
+                if len(b) < 5:
+                    for point in b:
+                        #print(point)
+                        if tiles[point[1]][point[0]] == "#":
+                            blocked = True
+                else:
+                    blocked = True
+
+
+            if blocked:
+                stdscr.addstr(y, x, " ")
+            else:
+                stdscr.addstr(y, x, column)
+        # representation += "\n"
+
+while True:
+    key = stdscr.getkey()
+
+    if key == "KEY_UP":
+        player_y -= 1
+    elif key == "KEY_DOWN":
+        player_y += 1
+    elif key == "KEY_LEFT":
+        player_x -= 1
+    elif key == "KEY_RIGHT":
+        player_x += 1
+
+    draw_stuff()
+    stdscr.addstr(player_y, player_x, "@")
+    stdscr.refresh()
+
 
 #
 # tiles[room_start] = "."
